fe82c1f8cfaa3d915cc58ffc67bf2afa7d32444f
Douglas Paz <eu@douglaspaz.com>
2026-04-19 13:57:08 -0300

    feat: add status link

--- src/components/pages/base.js
+++ src/components/pages/base.js
@@ -39,6 +39,7 @@ export default (content, { title, q } = {}) => (`<!doctype html>
   <footer>
     <ul>
       <li><a href="https://betor.top" target="_blank">BeTor</a></li>
+      <li><a href="https://grafana.betor.top/public-dashboards/1019100df7a84d9daca507137f3b3e51" target="_blank">Status</a></li>
       <li><a href="https://github.com/dougppaz/betor-catalog" target="_blank">Github - betor-catalog</a></li>
     </ul>
     <p><strong>Contato/Dúvidas:</strong> betor@betor.top</p>
